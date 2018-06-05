6f79fa01df608fbfe541adcc8045adaf96bb29ab
lisa <l.marival@yahoo.fr>
2018-06-05 12:54:30 +0200

    listerecette

--- back/app.js
+++ back/app.js
@@ -20,57 +20,10 @@ app.use(session({ secret: "que j'aime à faire apprendre une nombre utile aux sa
 mongoose.connect("mongodb://admin:admin123@ds241530.mlab.com:41530/mongo-test");
 
 app.use('', userRoute)
-app.get("/", (req, res) => {
-    res.render("home");
-})
 
 
-app.get('/recipe-load', (req, res) => {
-    /* let theme= new models.theme({
-         title: "Cuisine exotique"
-     });*/
-    let theme1 = new models.theme({
-        title: "Cuisine exotique"
-    });
 
 
-    let recipe = new models.recipe({
-        title: 'Poulet au curry',
-        cookingTime: 45,
-        PrepareTime: 20,
-        theme: [theme1._id],
-        difficulty: 3,
-        nbPerson: 4
-    });
-
-    let recipe1 = new models.recipe({
-        title: 'Tarte à la Rhubarbe',
-        cookingTime: 30,
-        PrepareTime: 15,
-        theme: [recipe._id],
-        difficulty: 4,
-        nbPerson: 4
-    });
-    //pour enregistrer
-
-    recipe.save((err, data) => {
-        if (err) {
-            console.log(err);
-            res.end(err);
-        } else {
-            res.json(data);
-        }
-    });
-
-    /*recipe1.save((err, data)=>{
-        if(err){
-            console.log(err);
-            res.end(err);
-        } else {
-            res.json(data);
-        }
-    }); */
-});
 
 
 //chemin pour afficher la liste
--- back/routes/user-routes.js
+++ back/routes/user-routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Model = require('./../model');
 
 router.get('/', (req, res) => {
-    recipeModel.find({}, (err, data) => {
+    Model.recipe.find({}, (err, data) => {
         if (err) {
             console.log(err);
             res.end('Erreur Mongoose');
@@ -14,28 +14,4 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/form*', (req, res) => {
-    console.log(req.body);
-    let recipe = {
-        title: req.body.title,
-        cookingTime:req.body.cookingTime,
-        PrepareTime:req.body.PrepareTime,
-        theme:req.body.theme,
-        difficulty:req.body.difficulty,
-        nbPerson:req.body.nbPerson,
-    };
-});
-
-
-router.get('/delete/:id', (req,res)=>{
-    model.remove({_id:req.params.id},(err)=>{
-        if (err){
-            console.log(err);
-            res.end('erreur');
-        }else {
-            res.redirect('/home');
-        }
-        
-    })
-})
 module.exports=router;
